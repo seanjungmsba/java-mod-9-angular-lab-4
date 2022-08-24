21d78dc54bfe5ba423710b0b1ae6a8076e6322dc
seanjungmsba <seanjeong1996@gmail.com>
2022-08-24 19:08:10 -0400

    Completed 'Unit Testing in Angular' module

--- flatiron-ng-messenger/src/app/header/header.component.spec.ts
+++ flatiron-ng-messenger/src/app/header/header.component.spec.ts
@@ -2,22 +2,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+// describe('HeaderComponent', () => {
+//   let component: HeaderComponent;
+//   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ HeaderComponent ]
+//     })
+//     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+//     fixture = TestBed.createComponent(HeaderComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+describe("'describe()' is a Jasmine function that lets us define a test suite", () => {
+  let testRunCount: number = 0;
+
+  beforeEach(() => {
+    testRunCount++;
+    console.log("'beforeEach()' has run " + testRunCount + " time(s)");
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("'it()' is a Jasmine function that lets us define a test case", () => {
+    expect(true).toBe(true);
+  });
+
+  it("should fail in order to demonstrate what a test failure looks like in Karma", () => {
+    expect(false).toBe(true);
   });
 });
