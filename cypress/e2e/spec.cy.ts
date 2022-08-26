29262266dca05de8614be2c069ae729bf164c453
seanjungmsba <seanjeong1996@gmail.com>
2022-08-25 20:27:50 -0400

    Cypress test working

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,10 @@
 describe('empty spec', () => {
+
   const appURL = 'http://localhost:4200/';
-  const testMessage = 'cypress test message'; //
+  const testMessage = 'cypress test message';
 
   it('passes', () => {
+
     cy.visit(appURL);
 
     cy.contains('New Message').click();
@@ -11,8 +13,15 @@ describe('empty spec', () => {
     cy.contains('Start Message').click();
     cy.url().should('equal', appURL);
 
-    cy.get('input[name=user-message-input]').type(testMessage); // get the input box by it 'name' attribute and type in it
-    cy.contains('Send').click(); // "Send" the message
-    cy.get('app-conversation-thread-component').should('contain', testMessage); // check that the message was sent
+    // get the input box by it 'name' attribute and type in it
+    cy.get('input[name=user-message-input]').type(testMessage);
+
+    // "Send" the message
+    cy.contains('Send').click();
+
+    // check that the message was sent
+    cy.get('app-conversation-thread-component').should('contain', testMessage);
+
   });
+  
 });
