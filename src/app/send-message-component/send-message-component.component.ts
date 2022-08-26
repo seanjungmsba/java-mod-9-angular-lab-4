5f3556d5107785eae14a2e700b11b78cf2b4f5cd
seanjungmsba <seanjeong1996@gmail.com>
2022-08-26 11:14:42 -0400

    Documented send-message component

--- src/app/send-message-component/send-message-component.component.ts
+++ src/app/send-message-component/send-message-component.component.ts
@@ -10,28 +10,40 @@ import { MessagingDataService } from '../messaging-data.service';
   styleUrls: ['./send-message-component.component.css'],
 })
 export class SendMessageComponentComponent implements OnInit {
+
+  // messageString is a two-way binded variable ( [(ngModel)]="messageString" )
   messageString: string;
   sequenceNumber = 1;
 
+  // why do we inject MessagingDataService? => we need to add message using addUserMessage() method available in this service
   constructor(
     private loggingSvce: LoggingService,
-    private messagingSvce: MessagingDataService
-  ) {}
+    private messagingSvce: MessagingDataService) {}
 
   ngOnInit(): void {}
 
   // use the instance of the logging service in our event handler
   onSendMessage() {
+
+    // set currentUser
     let currentUser: User = { firstName: 'Maria', isOnline: true };
 
+    // display messageString
     this.loggingSvce.log(this.messageString);
+
+    // contruct newMessage
     const newMessage: Message = new Message(
       currentUser,
       this.messageString,
       1,
       this.sequenceNumber
     );
+
+    // increment sequenceNumber
     this.sequenceNumber++;
+
+    // adding user message
     this.messagingSvce.addUserMessage(newMessage);
+
   }
 }
