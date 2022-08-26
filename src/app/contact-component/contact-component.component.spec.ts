5dbb092705c3a272a938221d7b753c15e5f5eb6d
seanjungmsba <seanjeong1996@gmail.com>
2022-08-26 10:08:57 -0400

    Deleted unnecessary spec file

--- src/app/contact-component/contact-component.component.spec.ts
+++
@@ -1,3 +0,0 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContactComponentComponent } from './contact-component.component';
--- src/app/contact-list-component/contact-list-component.component.spec.ts
+++
@@ -1,3 +0,0 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContactListComponentComponent } from './contact-list-component.component';
